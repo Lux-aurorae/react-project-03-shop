456a50440fee0a0b8711730fcf949591a8526866
Lux-aurorae <sherlock216@naver.com>
2026-08-19 00:15:04 +0900

    React)Third-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
-import NavBar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import ProductAllPage from "./pages/ProductAllPage";
 import LoginPage from "./pages/LoginPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
@@ -8,10 +8,10 @@ import ProductDetailPage from "./pages/ProductDetailPage";
 function App() {
   return (
     <>
-      {/* NavBar는 <Routes> 바깥에 있으므로 페이지가 바뀌어도 그대로 남습니다.
-          각 페이지마다 NavBar를 넣으면 중복이 생기고, 페이지 전환 때마다
+      {/* Navbar는 <Routes> 바깥에 있으므로 페이지가 바뀌어도 그대로 남습니다.
+          각 페이지마다 Navbar를 넣으면 중복이 생기고, 페이지 전환 때마다
           네비게이션이 다시 그려져 깜빡입니다. */}
-      <NavBar />
+      <Navbar />
 
       <main className="page">
         <Routes>
